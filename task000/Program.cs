b0ee3b76be78e3726db373bd25c2c7cb8b2378c8
Andrei Jermakov <jermakov.andrei@gmail.com>
2021-12-20 23:53:41 +0200

    task 00000

---
+++ task000/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывести квадрат числа
+
+Console.Write("vvedite cislo: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("{0} v kvadrate {1}", a, Math.Pow(a, 2));
+Console.ReadLine();
